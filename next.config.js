b47ae9e4234bd4da2057c79cf3a00dc4117e0866
Hoang Thu <thu.hoang@codeforcevina.com>
2023-06-16 09:23:38 +0700

    ssr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
+    // output: "export",
     images: {
         domains: ['cdn.resales-online.com', 'resource.resales-online.com'],
         unoptimized: true
